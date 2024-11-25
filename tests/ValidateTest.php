1205b9d1615535f9c14a9bbce545610c79c3cbdd
thinkphp <thinkphp@qq.com>
2024-11-25 22:04:26 +0800

    调整

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -102,7 +102,7 @@ class ValidateTest extends TestCase
 
         $this->assertFalse($result);
         $this->assertEquals('accept must be no,off,false or 0', $validate->getError());
-        $this->assertEquals(['accept' => 'accept must be no,off,false or 0'], $validate->getError());
+        $this->assertEquals(['accept' => 'accept must be no,off,false or 0'], $validate->getError(true));
     }
 
     public function testMultipleOf()
